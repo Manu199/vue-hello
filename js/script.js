0ed84d3d3ffcd35f3cf5b9ce243597f966fedb68
Manu199 <manu199gh@gmail.com>
2023-09-27 15:34:52 +0200

    [print] Vue data

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp({
     data(){
         return {
             helloWorld: 'Hello World!',
-            placeHolderImage: require('https://loremflickr.com/640/360'),
+            placeHolderImage: 'img/pexels-pixabay-36717.jpg',
         }
     }
 
